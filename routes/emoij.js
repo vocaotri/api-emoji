dbebc6965de83396c030a4e3729976ff4e6988e6
Tri Vo <vocaotri445@gmail.com>
2021-03-12 10:22:18 +0700

    set status

--- routes/emoij.js
+++ routes/emoij.js
@@ -6,37 +6,43 @@ const emoijRoutes = (app, fs) => {
       if (err) {
         throw err;
       }
+      res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.header("Pragma", "no-cache");
+      res.header("Expires", 0);
       var key = req.jsonResult
       var cacheBody = cache.get(key);
       if (cacheBody) {
         res.send(cacheBody);
       } else {
         var limit = parseInt(req.query.limit ?? 10);
-      var keyword = req.query.name ?? null;
-      var totalItem = 0;
-      var totalPage = 0;
-      var currentPage = parseInt(req.query.page ?? 1);
-      jsonResult = JSON.parse(data);
-      if (keyword) {
-       jsonResult = jsonResult.filter(i=>i.name.indexOf(keyword) > -1)
-      }
-      totalItem = jsonResult.length;
-      totalPage = Math.ceil(totalItem / limit);
-      jsonResult = jsonResult.slice(
-        limit * currentPage - limit,
-        limit * currentPage
-      );
+        var keyword = req.query.name ?? null;
+        var totalItem = 0;
+        var totalPage = 0;
+        var currentPage = parseInt(req.query.page ?? 1);
+        jsonResult = JSON.parse(data);
+        if (keyword) {
+          jsonResult = jsonResult.filter(i => i.name.indexOf(keyword) > -1)
+        }
+        totalItem = jsonResult.length;
+        totalPage = Math.ceil(totalItem / limit);
+        jsonResult = jsonResult.slice(
+          limit * currentPage - limit,
+          limit * currentPage
+        );
+        res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+        res.header("Pragma", "no-cache");
+        res.header("Expires", 0);
         var body = {
-        limit: limit,
-        totalPage: totalPage,
-        totalItem: totalItem,
-        currentPage: currentPage,
-        data: jsonResult,
+          limit: limit,
+          totalPage: totalPage,
+          totalItem: totalItem,
+          currentPage: currentPage,
+          data: jsonResult,
+        }
+        cache.put(key, body)
+        res.status(200).send(body);
       }
-        cache.put(key,body)
-      res.status(200).send(body);
-      }
-      
+
     });
   });
 };
