4073e5a1a10f4ae19a569d4c89606c35ce586f21
Tri Vo <vocaotri445@gmail.com>
2021-03-12 11:32:39 +0700

    update code

--- routes/emoij.js
+++ routes/emoij.js
@@ -3,23 +3,19 @@ var emoijService = require('../service/emoij.service');
 const emoijRoutes = (app, fs) => {
   const dataPath = "./data/emoji.json";
   app.get("/emojis", (req, res) => {
-    fs.readFile(dataPath, "utf8", (err, data) => {
-      if (err) {
-        throw err;
-      }
-      res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-      res.header("Pragma", "no-cache");
-      res.header("Expires", 0);
-      var key = req.query;
-      var cacheBody = cache.get(key);
-      if (cacheBody) {
-        res.send(cacheBody);
-      } else {
-        var body = emoijService.emoij(req, data);
-        cache.put(key, body)
-        res.status(200).send(body);
-      }
-    });
+    var data = fs.readFileSync(dataPath, "utf8");
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+    var key = JSON.stringify(req.query);
+    var cacheBody = cache.get(key);
+    if (cacheBody) {
+      res.send(cacheBody);
+    } else {
+      var body = emoijService.emoij(req, data);
+      cache.put(key, body)
+      res.send(body);
+    }
   });
 };
 
