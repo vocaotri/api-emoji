f9adb1da6058403b2fcae8f67cf7b4aabb18eb5e
Tri Vo <vocaotri445@gmail.com>
2021-03-12 09:04:39 +0700

    update search name

--- routes/emoij.js
+++ routes/emoij.js
@@ -6,17 +6,21 @@ const emoijRoutes = (app, fs) => {
         throw err;
       }
       var limit = parseInt(req.query.limit ?? 10);
+      var keyword = req.query.name ?? null;
       var totalItem = 0;
       var totalPage = 0;
       var currentPage = parseInt(req.query.page ?? 1);
       jsonResult = JSON.parse(data);
+      if (keyword) {
+       jsonResult = jsonResult.filter(i=>i.name.indexOf(keyword) > -1)
+      }
       totalItem = jsonResult.length;
       totalPage = Math.ceil(totalItem / limit);
       jsonResult = jsonResult.slice(
         limit * currentPage - limit,
         limit * currentPage
       );
-      res.send({
+      res.status(200).send({
         limit: limit,
         totalPage: totalPage,
         totalItem: totalItem,
