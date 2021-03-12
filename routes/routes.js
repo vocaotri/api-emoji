1a2d4b5a28e0a8e0547fc7476c9d4eae3790eace
Tri Vo <vocaotri445@gmail.com>
2021-03-12 09:40:09 +0700

    update status code

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 var path = require('path');
 const userRoutes = require("./emoij");
 const appRouter = (app, fs) => {
-  app.get("/", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,'..','views/index.html'));
   });
   userRoutes(app, fs);
